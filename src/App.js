f78d884d7229cd5e0ca23462de58a00b8b54ae26
kimja7045 <kimja7045@naver.com>
2020-01-02 08:48:52 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 웹사이트의 화면에 대한 내용 출력 담당
+
+class App extends Component {
+  render() {
+    return (
+      <div className="gray-background">
+        <img src={logo} alt="logo"/>
+        <h2>Let's develop management system!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
